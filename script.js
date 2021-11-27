c863512100d6a6fba77570c794b078be1ef01e1e
Zekel <13670661+ZZ83@users.noreply.github.com13670661+ZZ83@users.noreply.github.com>
2021-11-27 14:19:40 -0800

    Add main navigation

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ navToggle.addEventListener('click', () => {
     const vis = mainNav.getAttribute("data-visible");
     if (vis === "false") {
         mainNav.setAttribute("data-visible", true);
-        
+        navToggle.setAttribute("aria-expanded", true);
     } else if (vis === "true") {
         mainNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
     }
-
-    console.log(vis)
 })
